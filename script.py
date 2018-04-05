8fd418850b37e8bde5e34a220c099720fee95e71
maker <you@example.com>
2018-04-05 08:37:16 +0300

    Add some changes

--- script.py
+++ script.py
@@ -4,15 +4,13 @@ import paramiko
 
 
 def firstdiag():
-
-
     data = input('Enter site: ')
     ping = (os.system('ping -c 2 ' + data  ))
     dig = (os.system('dig ANY ' + data +' |sed -n "/ANSWER SECTION:/,/Query time:/p"'))
     nslookup = (os.system('whois ' + data +' | sed -n "/domain/,/source/p" ' ))
     invite = input('Enter to user folder? - y\nQuit - q: ')
     if invite == 'y':
-        
+        pass
     if invite == 'q':
         pass
 
