dda5341426b6d5de8e4db52acfd07fc05177932e
maker <you@example.com>
2018-04-05 09:47:07 +0300

    Changes

--- script.py
+++ script.py
@@ -1,21 +1,15 @@
 import os
-import paramiko
 
-
-
-def firstdiag():
-    data = input('Enter site: ')
+#Return console command's: ping, dig ANY and whois.
+def firstdiag(data):
     ping = (os.system('ping -c 2 ' + data  ))
     dig = (os.system('dig ANY ' + data +' |sed -n "/ANSWER SECTION:/,/Query time:/p"'))
     nslookup = (os.system('whois ' + data +' | sed -n "/domain/,/source/p" ' ))
-    invite = input('Enter to user folder? - y\nQuit - q: ')
-    if invite == 'y':
-        pass
-    if invite == 'q':
-        pass
+    invite = input('Press 1 to ping\nPress2 to dig ANY\nPress 3 to nslookup')
 
-def gotobackend():
-    pass
+
+
 
 if __name__ == '__main__':
-    firstdiag()
+    data = input('Enter site: ')
+    firstdiag(data)
